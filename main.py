008e06e8b3b997217f5c46112225c21184d76b3d
Екатерина <sokolova0ekaterina@gmail.com>
2024-10-03 20:25:03 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from math import sqrt
+
+def solve_quadratic_equation(a: float, b: float, c: float) -> list:
+    try:
+        if a == 0:
+            return [c / b]
+
+        discr = b * b - 4 * a * c
+
+        if discr > 0:
+            x1 = (-b + sqrt(discr)) / (2 * a)
+            x2 = (-b - sqrt(discr)) / (2 * a)
+            return [x1, x2]
+        elif discr < 0:
+            return ["нет корней"]
+        elif discr == 0:
+            return [-b / (2 * a)]
+    except:
+        ["введены неверные параметры функции"]
+
+
+
+def main():
+    result = solve_quadratic_equation(1, -4, -5)
+
+    for i in range(len(result)):
+        if type(result[i]) == float:
+            print(f"x{i+1} = {result[i]}")
+        else:
+            print(f"{result[i]}")
+
+
+if __name__ == '__main__':
+    main()
