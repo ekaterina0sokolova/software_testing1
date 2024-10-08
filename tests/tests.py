4177d11fbbe62681d466c795e1b09aa35ddadb5f
Екатерина <sokolova0ekaterina@gmail.com>
2024-10-08 21:08:58 +0300

    eeeeeeeeeeeeee

--- tests/tests.py
+++ tests/tests.py
@@ -12,6 +12,7 @@ class TestQuadraticEquationModule(unittest.TestCase):
     def test_negative_discr(self):
         self.assertEqual(solve_quadratic_equation(a=2, b=-1, c=1), ["нет корней"])
 
+    # hello
     def test_zero_a(self):
         self.assertEqual(solve_quadratic_equation(a=0, b=2, c=-4), [2.0])
 
